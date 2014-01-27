703c0b433b6197515b5b06ed8cf9fe073e9894d8
elena <elena.borovik@gameloft.com>
2014-01-27 13:11:18 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ManagerTool::Application.config.secret_key_base = '4f2f9c1584295f13bb71bd535862c75dbe681d1895e284cd6d16b91c34917e989fdc9ededb494efb760457eb8fe07c804952dd0e062349cee37dc553a4047ca0'
